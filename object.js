24b123baa036a6df6c2edbe527a008f30488eff3
AvijeetAk <avijeetkumar1994@gmail.com>
2023-09-15 03:44:11 +0530

    main

---
+++ object.js
@@ -0,0 +1,11 @@
+const student = {
+    name: 'Avijeet',
+    doj : 'Jan',
+
+    printdetails() {
+        console.log('Hi I am ' + this.name + '. I joined in ' + this.doj);
+  
+    }
+};
+
+student.printdetails();

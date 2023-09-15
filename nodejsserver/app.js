bec34792c43e062719a5604824f49cf0e8e1ad10
AvijeetAk <avijeetkumar1994@gmail.com>
2023-09-15 17:51:54 +0530

    main

--- nodejsserver/app.js
+++ nodejsserver/app.js
@@ -3,7 +3,55 @@ const http = require('http');
 
 
 const server = http.createServer((req,res) => {
-    console.log("Avijeet");
+    
+   // process.exit();
+
+   const url = req.url;
+
+   if(url === '/')
+   {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>intro</title></head>')
+    res.write('<body>Welcome to my node js project</body>')
+    res.write('</html>')
+    res.end();
+
+   }
+   if(url === '/home')
+   {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>homepage</title></head>')
+    res.write('<body>Welcome home</body>')
+    res.write('</html>')
+    res.end();
+
+   }
+
+   if(url === '/about')
+   {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>about</title></head>')
+    res.write('<body>Welcome to About Page</body>')
+    res.write('</html>')
+    res.end();
+
+   }
+
+   if(url === '/node')
+   {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>homepage</title></head>')
+    res.write('<body>Welcome to my node js project</body>')
+    res.write('</html>')
+    res.end();
+
+   }
+
+
 });
 
 server.listen(4000);

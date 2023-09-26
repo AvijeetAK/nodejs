4ebe048f1ce4180c29dadb64636da45057f9f89e
AvijeetAk <avijeetkumar1994@gmail.com>
2023-09-26 12:01:53 +0530

    main

--- nodejsserver/routes.js
+++ nodejsserver/routes.js
@@ -8,25 +8,27 @@ const requestHandler = (req,res) => {
 
     if(url === '/')
 {
-   var datanew = "";
+   
 
-   fs.readFile("message.txt", {encoding : 'utf-8'}, (data, err) =>{
+   fs.readFile("message.txt", {encoding : 'utf-8'}, (err, data) =>{
 
      
-      
+     if(err){
+        console.log(err);
+     }
+
       console.log('data from file' + data);
       res.write('<html>');
-      res.write('<head><title>Enter Message <title><head>');
-      res.write(`<body>${datanew}</body>`);
-      res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button><form> </body>')
+      res.write('<head><title>Enter Message </title></head>');
+      res.write(`<body>${data}<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form> </body>`)
       res.write('</html>');
+    
+      return res.end();
      
       
     })
 
-    
-    
-    return res.end();
+   
      
 
 
@@ -46,25 +48,17 @@ if(url === "/message"  &&  method === "POST")
       const parseBody = Buffer.concat(body).toString();
       const message = parseBody.split("=")[1];
       fs.writeFileSync("message.txt", message);
-      redmsg = message;
+      
 
 
    })
-
-   res.setHeader('COntent-Type', 'application/json');
-   res.write(redmsg);
 
    res.statusCode = 302;
    res.setHeader("Location", "/");
    return res.end();
 }
 
- res.setHeader('Content-Type', 'text/html');
- res.write('<html>');
- res.write('<head><title>My First Page</title></head>')
- res.write('<body>Hello from the node.js server</body>')
- res.write('</html>')
- res.end();
+
 
 }
 
